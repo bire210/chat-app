ec045cc03dcc37b9601a85a2a2f132078622f13b
Birendra Mahto <divyabiru27@gmail.com>
2024-12-30 07:54:15 +0530

    big issue resolved

--- chatapp/src/api/apiInstance.js
+++ chatapp/src/api/apiInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://chat-app-udbk.onrender.com/api/v1";
+const BASE_URL = "https://chat-backend-v1sj.onrender.com/api/v1";
 // const BASE_URL = "http://localhost:8000/api/v1";
 const AxiosInstance = axios.create({ baseURL: BASE_URL });
 
